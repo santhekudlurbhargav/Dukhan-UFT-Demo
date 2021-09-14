81b057da1059d800ef96e7c8bb8b093f7c446da6
Bhargav Reddy <90080816+santhekudlurbhargav@users.noreply.github.com>
2021-09-14 08:54:49 +0530

    Delete GUITest3 directory
    unwanted script

--- GUITest3/Res6/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Test\\Desktop\\UFTRecord&PlayBack\\Scripts\\GUITest3\\Res6',namePairs:[]}]};
